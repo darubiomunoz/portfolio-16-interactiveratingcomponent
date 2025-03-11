07d89114306da9e72b7651a483f1aeb7bd502ace
Diego Rubio - darmdev <darubiomunoz@gmail.com>
2025-03-10 19:34:27 -0700

    🔨 [rating component]: create the logic for the rating component

---
+++ src/components/RatingComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RatingComponent = () => {
+    return (
+        <div>
+            <h1>Rating Component</h1>
+        </div>
+    );
+};
+
+export { RatingComponent };
