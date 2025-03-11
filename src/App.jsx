8c84b6a32d540f86d1bf0d96e15f5b012430303d
Diego Rubio - darmdev <darubiomunoz@gmail.com>
2025-03-10 19:33:11 -0700

    🔨 [App]: created the logic for ahe App file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RatingComponent } from './components/RatingComponent';
+
+const App = () => {
+    return (
+        <div className="App">
+            <RatingComponent />
+        </div>
+    );
+};
+
+export default App;
