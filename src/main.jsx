0291c07c691b3b9fadd4e8e7364cf767c5963705
Diego Rubio - darmdev <darubiomunoz@gmail.com>
2025-03-10 20:25:43 -0700

    💄 [overpass font]: implementing the desired font into the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/reset.css';
-import './styles/variables.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
