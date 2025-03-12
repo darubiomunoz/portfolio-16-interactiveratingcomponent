350eb05abe42f1f778e9d597bfd62cfdf1ee2619
Diego Rubio - darmdev <darubiomunoz@gmail.com>
2025-03-11 20:32:38 -0700

    🔨 [app.jsx logic]: improved the logic of the ap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './styles/App.css';
+import { AppContext } from './AppContext';
 import { RatingComponent } from './components/RatingComponent';
+import { ThankYouComponent } from './components/ThankYouComponent';
 
 const App = () => {
+    const {
+        submitted
+    } = useContext(AppContext);
+
     return (
         <div className="App">
-            <RatingComponent />
+            {!submitted ? (
+                <RatingComponent />
+            ) : (
+                <ThankYouComponent />
+            )}
         </div>
     );
 };
