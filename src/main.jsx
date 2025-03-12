a5a6b94a38391a923e7ccae2724c27df088f5533
Diego Rubio - darmdev <darubiomunoz@gmail.com>
2025-03-11 19:26:53 -0700

    🏗️ [app provider]: wrapped the app with its provider on the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import App from './App';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { AppProvider } from './AppContext';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
     <StrictMode>
-        <App />
+        <AppProvider>
+            <App />
+        </AppProvider>
     </StrictMode>
 );
